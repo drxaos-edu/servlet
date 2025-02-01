ee15fb8081d64c2b131bc2e8eec6f2736086ee61
v.polyakov <v.polyakov@dats.team>
2025-02-01 23:46:56 +0300

    init

---
+++ src/main/java/com/example/servlet/MainServlet.java
@@ -0,0 +1,20 @@
+package com.example.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("name", "World");
+        req.getRequestDispatcher("mypage.jsp").forward(req, resp);
+    }
+}
